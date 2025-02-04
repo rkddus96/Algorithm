536aceb639fa6375f546287fb339573d212e830f
rkddus96 <103873698+rkddus96@users.noreply.github.com>
2025-02-04 09:46:15 +0900

    [level 1] Title: 문자열 나누기, Time: 0.04 ms, Memory: 4.07 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    int xCount = 0;
+    int notxCount = 0;
+    char x = s[0];
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == x)
+            xCount++;
+        else
+            notxCount++;
+        
+        if (xCount == notxCount || i == s.size()-1)
+        {
+            answer++;
+            if (i < s.size()-1)
+                x = s[i+1];
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
+    return answer;
+}
