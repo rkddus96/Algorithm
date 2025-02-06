c0d44396db6483051397550bc826f4d99e2f34e5
rkddus96 <103873698+rkddus96@users.noreply.github.com>
2025-02-06 09:31:37 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, string skip, int index) {
+    string answer = "";
+    vector<bool> skipAlphabets(26, false);
+    for (char c : skip)
+    {
+        skipAlphabets[c-'a'] = true;
+    }
+    
+    for (char c : s)
+    {
+        int shiftCount = 0;
+        int stringIndex = c-'a';
+        while (shiftCount < index)
+        {
+            stringIndex = (stringIndex+1)%26;
+
+            if (skipAlphabets[stringIndex])
+            {
+                continue;
+            }
+            shiftCount++;
+        }
+        answer += stringIndex + 'a'; 
+        
+    }
+    return answer;
+}
