01e99c2fd61ecb41c9e7ba20611440dd253c8c7f
rkddus96 <103873698+rkddus96@users.noreply.github.com>
2026-03-26 11:43:50 +0900

    [Silver IV] Title: 스택, Time: 216 ms, Memory: 2196 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    vector<int> s;
+    
+    while (t--)
+    {
+        string action;
+        cin >> action;
+        
+       if (action == "push") {
+            int element;
+            cin >> element;
+            s.push_back(element);
+        }
+        else if (action == "pop") {
+            if (s.empty()) cout << -1 << '\n';
+            else {
+                cout << s.back() << '\n';
+                s.pop_back();
+            }
+        }
+        else if (action == "top") {
+            if (s.empty()) cout << -1 << '\n';
+            else cout << s.back() << '\n';
+        }
+        else if (action == "size") {
+            cout << s.size() << '\n';
+        }
+        else if (action == "empty") {
+            cout << (s.empty() ? 1 : 0) << '\n';
+        }
+    }
+}
